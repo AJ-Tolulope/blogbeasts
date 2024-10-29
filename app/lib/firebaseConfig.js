b46959a34b912adc68db8253e73c66a9c73fe4b4
ajalatolulope <84034486+AJ-Tolulope@users.noreply.github.com>
2024-10-29 17:37:18 +0100

    changes

--- app/lib/firebaseConfig.js
+++ app/lib/firebaseConfig.js
@@ -1,20 +1,21 @@
-// firebase.js
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCQYGE2802HwFyUYDtW_4lCE_E1Nx71Ybs",
+  authDomain: "blog-beasts.firebaseapp.com",
+  projectId: "blog-beasts",
+  storageBucket: "blog-beasts.appspot.com",
+  messagingSenderId: "121970347733",
+  appId: "1:121970347733:web:99070b351da55d5d971c5c",
+  measurementId: "G-M2X40PZP45",
 };
 
-// Initialize Firebase only if not already initialized
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-// Initialize Firebase services
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export { db, auth };
